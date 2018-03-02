deac6dd669abcfc93ee5b4d5723befa7f3bf6dff
HP PC <HP PC@HP>
2018-03-02 11:49:13 +0200

    first commit

---
+++ test/src/test/java/test1/runtestes.java
@@ -0,0 +1,17 @@
+package test1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class runtestes {
+	@Test
+	public void Dummy_test_to_pass(){
+		Assert.assertEquals(1, 1);
+		
+	}
+	@Test
+	public void Dummy_test_to_fail(){
+		Assert.assertEquals(1, 2);
+		
+	}
+}
